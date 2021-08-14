c9764002544ed32a9f54f74f4d5229086c9dcb0c
Constantin <constantinmensendiek@gmail.com>
2021-08-14 18:09:33 +0200

    added enpoint /planner/suggested-recipes

--- backend/app/controller/planner/planner_controller.py
+++ backend/app/controller/planner/planner_controller.py
@@ -6,6 +6,7 @@ from app import app
 from app.helpers import validate_args
 from app.models import Recipe, RecipeHistory
 from .schemas import AddPlannedRecipe
+from random import randint
 
 
 @app.route('/planner/recipes', methods=['GET'])
@@ -22,9 +23,10 @@ def addPlannedRecipe(args):
     recipe = Recipe.find_by_id(args['recipe_id'])
     if not recipe:
         raise NotFoundRequest()
-    recipe.planned = True
-    recipe.save()
-    RecipeHistory.create_added(recipe)
+    if not recipe.planned:
+        recipe.planned = True
+        recipe.save()
+        RecipeHistory.create_added(recipe)
     return jsonify(recipe.obj_to_dict())
 
 
@@ -34,9 +36,10 @@ def removePlannedRecipeById(id):
     recipe = Recipe.find_by_id(id)
     if not recipe:
         raise NotFoundRequest()
-    recipe.planned = False
-    recipe.save()
-    RecipeHistory.create_dropped(recipe)
+    if recipe.planned:
+        recipe.planned = False
+        recipe.save()
+        RecipeHistory.create_dropped(recipe)
     return jsonify(recipe.obj_to_dict())
 
 
@@ -45,3 +48,28 @@ def removePlannedRecipeById(id):
 def getRecentRecipes():
     recipes = RecipeHistory.get_recent()
     return jsonify([e.recipe.obj_to_dict() for e in recipes])
+    
+@app.route('/planner/suggested-recipes', methods=['GET'])
+def getSuggestedRecipes():
+    # get all unplanned recipes with positive suggestion_score
+    recipes = Recipe.query.filter(Recipe.planned == False).filter(Recipe.suggestion_score != 0).all()
+    # compute the initial sum of all suggestion_scores
+    suggestion_sum = 0
+    for r in recipes:
+        suggestion_sum += r.suggestion_score
+    # randomly suggest one recipe weighted by their score   
+    suggested_recipes = []
+    while len(suggested_recipes) < 9 and len(recipes) > 0:
+        choose = randint(1,suggestion_sum)
+        to_be_removed = -1
+        for (i,r) in enumerate(recipes):
+            choose -= r.suggestion_score
+            if choose <= 0:
+                suggested_recipes.append(r)
+                suggestion_sum -= r.suggestion_score
+                to_be_removed = i
+                break
+        recipes.pop(to_be_removed)
+    # jsonfy suggestions
+    return jsonify([r.obj_to_dict() for r in suggested_recipes])
+

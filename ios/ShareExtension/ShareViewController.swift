c2c63b2024be16fc11c981911ee93c4dd418e5bf
Tom Bursch <tombursch@gmail.com>
2022-08-24 18:51:47 +0200

    ios: support

---
+++ ios/ShareExtension/ShareViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by Tom Bursch on 24.08.22.
+//
+
+import share_handler_ios_models
+
+class ShareViewController: ShareHandlerIosViewController {}

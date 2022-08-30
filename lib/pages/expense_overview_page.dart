2e4a8203a2b88bbb5e99938224f6ea6bbd33fba0
Tom Bursch <tombursch@gmail.com>
2022-08-30 15:06:59 +0200

    Update

--- lib/pages/expense_overview_page.dart
+++ lib/pages/expense_overview_page.dart
@@ -25,7 +25,7 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.list),
+        title: Text(AppLocalizations.of(context)!.overview),
       ),
       body: Align(
         alignment: Alignment.topCenter,
@@ -41,17 +41,20 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
               return ListView(
                 padding: const EdgeInsets.all(16),
                 children: [
+                  Text("This month by category"),
                   SizedBox(
                     height: 300,
                     child: charts.PieChart(
                       [_getSeriesCurrentMonth(state.categoryOverviewsByMonth)],
                       animate: true,
                       // defaultRenderer: charts.ArcRendererConfig(
-                      //     // arcWidth: 60,
-                      //     // arcRendererDecorators: [charts.ArcLabelDecorator()],
-                      //     ),
+                      //   // arcWidth: 60,
+                      //   // arcRendererDecorators: [charts.ArcLabelDecorator()],
+                      //   strokeWidthPx: 0,
+                      // ),
                     ),
                   ),
+                  Text("Previous months"),
                   SizedBox(
                     height: 300,
                     child: charts.BarChart(
@@ -59,6 +62,7 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
                       animate: true,
                       defaultRenderer: charts.BarRendererConfig(
                         groupingType: charts.BarGroupingType.stacked,
+                        stackedBarPaddingPx: 0,
                       ),
                     ),
                   ),
@@ -82,6 +86,7 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
       measureFn: (v, _) => v.value["0"],
       labelAccessorFn: (v, _) =>
           "${v.key}: ${NumberFormat.simpleCurrency().format(v.value["0"])}",
+      colorFn: (_, i) => _colorFn(i ?? 0),
     );
   }
 
@@ -89,14 +94,22 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
     Map<String, Map<String, double>> data,
   ) {
     return data.entries
-        .map((e) => charts.Series<MapEntry<String, double>, String>(
-              id: e.key,
-              data: e.value.entries.toList().reversed.toList(),
-              domainFn: (v, _) => _monthOffsetToString(int.tryParse(v.key)!),
-              measureFn: (v, _) => v.value,
-              labelAccessorFn: (v, _) =>
-                  "${_monthOffsetToString(int.tryParse(v.key)!)}: ${NumberFormat.simpleCurrency().format(v.value)}",
+        .toList()
+        .asMap()
+        .map((i, e) => MapEntry(
+              i,
+              charts.Series<MapEntry<String, double>, String>(
+                id: e.key,
+                data: e.value.entries.toList().reversed.toList(),
+                domainFn: (v, _) => _monthOffsetToString(int.tryParse(v.key)!),
+                measureFn: (v, _) => v.value,
+                labelAccessorFn: (v, _) =>
+                    "${_monthOffsetToString(int.tryParse(v.key)!)}: ${NumberFormat.simpleCurrency().format(v.value)}",
+                colorFn: (v, _) => _colorFn(i),
+              ),
             ))
+        .entries
+        .map((e) => e.value)
         .toList();
   }
 
@@ -104,5 +117,33 @@ class _ExpenseOverviewPageState extends State<ExpenseOverviewPage> {
     return DateFormat.MMMM()
         .dateSymbols
         .STANDALONEMONTHS[(DateTime.now().month - 1 - offset) % 12];
+  }
+
+  charts.Color _colorFn(int i) {
+    i = i % 5;
+    final l = List.generate(5, (i) {
+      Color c = lighten(Theme.of(context).colorScheme.primary, -0.2);
+      for (int j = 0; j < i; j++) {
+        c = lighten(c);
+      }
+
+      return c;
+    });
+
+    return charts.Color(
+      r: l[i].red,
+      g: l[i].green,
+      b: l[i].blue,
+    );
+  }
+
+  Color lighten(Color color, [double amount = 0.1]) {
+    assert(amount >= -1 && amount <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslLight =
+        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+    return hslLight.toColor();
   }
 }

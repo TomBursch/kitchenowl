c364fb09c9c6b59a0e2958d2c66c77294322690b
Christoph Loy <loy.christoph@gmail.com>
2022-11-13 19:16:31 +0100

    Fall back to standard url launcher on non-mobile platforms

--- lib/helpers/url_launcher.dart
+++ lib/helpers/url_launcher.dart
@@ -1,14 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart' as ul;
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 Future<void> openUrl(BuildContext context, String url) async {
-  await launch(
-    url,
-    customTabsOption: CustomTabsOption(
-      toolbarColor: Theme.of(context).primaryColor,
-      enableDefaultShare: true,
-    ),
-  );
+  return (Platform.isAndroid || Platform.isIOS)
+      ? await launch(
+          url,
+          customTabsOption: CustomTabsOption(
+            toolbarColor: Theme.of(context).primaryColor,
+            enableDefaultShare: true,
+          ),
+        )
+      : await ul.launchUrl(Uri.parse(url));
 }
 
 Future<bool> isValidUrl(String url) async {

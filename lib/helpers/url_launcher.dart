bce8c37cc876f78b62e0013e2db552cb49c80153
Tom Bursch <tombursch@gmail.com>
2022-11-16 00:01:38 +0100

    Update settings and fix web

--- lib/helpers/url_launcher.dart
+++ lib/helpers/url_launcher.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as ul;
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 Future<void> openUrl(BuildContext context, String url) async {
-  return (Platform.isAndroid || Platform.isIOS)
+  return (!kIsWeb && (Platform.isAndroid || Platform.isIOS))
       ? await launch(
           url,
           customTabsOption: CustomTabsOption(
-            toolbarColor: Theme.of(context).primaryColor,
+            toolbarColor: Theme.of(context).colorScheme.primary,
             enableDefaultShare: true,
+            extraCustomTabs: const [
+              'org.mozilla.firefox',
+              'com.microsoft.emmx',
+            ],
+          ),
+          safariVCOption: SafariViewControllerOption(
+            preferredBarTintColor: Theme.of(context).colorScheme.primary,
+            preferredControlTintColor: Theme.of(context).colorScheme.onPrimary,
+            barCollapsingEnabled: true,
+            entersReaderIfAvailable: false,
+            dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
           ),
         )
       : await ul.launchUrl(Uri.parse(url));

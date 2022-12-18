d941071db71505cea1f77b87c1c2a69a8600c17f
Minehub <contracts@minehub.de>
2022-12-18 23:57:29 +0100

    fix: remove ipv6 when unsupported (#110)

---
+++ docker-entrypoint-custom.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+ME=$(basename $0)
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+# if ipv6 is unavailable, remove it from the nginx template
+if [ ! -f "/proc/net/if_inet6" ]; then
+    entrypoint_log "$ME: info: ipv6 not available"
+    sed -i '/::/d' /etc/nginx/templates/default.conf.template
+fi

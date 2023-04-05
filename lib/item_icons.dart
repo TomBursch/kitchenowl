4355b899b0cb0d6e6c20f716df6510e3d9a720c2
Tom Bursch <tombursch@gmail.com>
2023-04-06 01:17:14 +0200

    feat: add more icons

--- lib/item_icons.dart
+++ lib/item_icons.dart
@@ -30,6 +30,8 @@ class ItemIcons {
     "bread_loaf": bread_loaf,
     "broccoli": broccoli,
     "cabbage": cabbage,
+    "can_soup": can_soup,
+    "candy_cane": candy_cane,
     "carrot": carrot,
     "celery": celery,
     "champagne_bottle": champagne_bottle,
@@ -56,8 +58,11 @@ class ItemIcons {
     "kohlrabi": kohlrabi,
     "leek": leek,
     "lettuce": lettuce,
+    "lipstick": lipstick,
+    "melon": melon,
     "milk_carton": milk_carton,
     "mushroom": mushroom,
+    "nappy": nappy,
     "nut": nut,
     "olive": olive,
     "onion": onion,
@@ -67,8 +72,10 @@ class ItemIcons {
     "pear": pear,
     "peas": peas,
     "penne": penne,
+    "perfume": perfume,
     "pills": pills,
     "pineapple": pineapple,
+    "plastic": plastic,
     "plastic_bottle": plastic_bottle,
     "plum": plum,
     "pomegranate": pomegranate,
@@ -77,18 +84,30 @@ class ItemIcons {
     "pumpkin": pumpkin,
     "radish": radish,
     "raspberry": raspberry,
+    "razor": razor,
     "salami": salami,
     "salami_pizza": salami_pizza,
     "sausage": sausage,
+    "shampoo": shampoo,
+    "shaving": shaving,
+    "soap_bubble": soap_bubble,
     "soy": soy,
     "soy_sauce": soy_sauce,
     "spinach": spinach,
+    "spray": spray,
     "strawberry": strawberry,
     "sweet_potato": sweet_potato,
     "tea": tea,
+    "tetra_pak": tetra_pak,
+    "toilet_paper": toilet_paper,
     "tomato": tomato,
+    "tooth_cleaning_kit": tooth_cleaning_kit,
+    "tube": tube,
+    "water": water,
+    "wet": wet,
     "wheat": wheat,
     "wine_bottle": wine_bottle,
+    "wipes": wipes,
   };
 
   static const IconData almond = IconData(256, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -106,63 +125,82 @@ class ItemIcons {
   static const IconData bread_loaf = IconData(268, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData broccoli = IconData(269, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData cabbage = IconData(270, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData carrot = IconData(271, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData celery = IconData(272, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData champagne_bottle = IconData(273, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData cheese = IconData(274, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData cherry = IconData(275, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData chocolate_bar = IconData(276, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData citrus = IconData(277, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData coconut = IconData(278, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData coffee_beans = IconData(279, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData coffee_capsule = IconData(280, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData cola = IconData(281, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData cookies = IconData(282, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData corn = IconData(283, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData cucumber = IconData(284, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData doughnut = IconData(285, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData eggplant = IconData(286, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData eggs = IconData(287, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData fish_food = IconData(288, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData garlic = IconData(289, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData ginger = IconData(290, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData grains_of_rice = IconData(291, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData ketchup = IconData(292, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData kiwi = IconData(293, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData kohlrabi = IconData(294, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData leek = IconData(295, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData lettuce = IconData(296, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData milk_carton = IconData(297, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData mushroom = IconData(298, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData nut = IconData(299, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData olive = IconData(300, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData onion = IconData(301, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData orange = IconData(302, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData paprika = IconData(303, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData peanuts = IconData(304, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pear = IconData(305, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData peas = IconData(306, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData penne = IconData(307, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pills = IconData(308, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pineapple = IconData(309, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData plastic_bottle = IconData(310, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData plum = IconData(311, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pomegranate = IconData(312, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData potato = IconData(313, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData prawn = IconData(314, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pumpkin = IconData(315, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData radish = IconData(316, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData raspberry = IconData(317, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData salami = IconData(318, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData salami_pizza = IconData(319, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData sausage = IconData(320, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData soy = IconData(321, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData soy_sauce = IconData(322, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData spinach = IconData(323, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData strawberry = IconData(324, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData sweet_potato = IconData(325, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData tea = IconData(326, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData tomato = IconData(327, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData wheat = IconData(328, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData wine_bottle = IconData(329, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData can_soup = IconData(271, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData candy_cane = IconData(272, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData carrot = IconData(273, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData celery = IconData(274, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData champagne_bottle = IconData(275, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cheese = IconData(276, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cherry = IconData(277, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chocolate_bar = IconData(278, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData citrus = IconData(279, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData coconut = IconData(280, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData coffee_beans = IconData(281, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData coffee_capsule = IconData(282, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cola = IconData(283, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cookies = IconData(284, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData corn = IconData(285, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cucumber = IconData(286, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData doughnut = IconData(287, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData eggplant = IconData(288, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData eggs = IconData(289, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData fish_food = IconData(290, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData garlic = IconData(291, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ginger = IconData(292, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData grains_of_rice = IconData(293, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ketchup = IconData(294, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData kiwi = IconData(295, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData kohlrabi = IconData(296, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData leek = IconData(297, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lettuce = IconData(298, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lipstick = IconData(299, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData melon = IconData(300, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData milk_carton = IconData(301, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mushroom = IconData(302, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData nappy = IconData(303, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData nut = IconData(304, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData olive = IconData(305, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData onion = IconData(306, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData orange = IconData(307, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData paprika = IconData(308, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData peanuts = IconData(309, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pear = IconData(310, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData peas = IconData(311, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData penne = IconData(312, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData perfume = IconData(313, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pills = IconData(314, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pineapple = IconData(315, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData plastic = IconData(316, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData plastic_bottle = IconData(317, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData plum = IconData(318, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pomegranate = IconData(319, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData potato = IconData(320, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData prawn = IconData(321, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pumpkin = IconData(322, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData radish = IconData(323, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData raspberry = IconData(324, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData razor = IconData(325, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData salami = IconData(326, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData salami_pizza = IconData(327, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sausage = IconData(328, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData shampoo = IconData(329, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData shaving = IconData(330, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData soap_bubble = IconData(331, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData soy = IconData(332, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData soy_sauce = IconData(333, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData spinach = IconData(334, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData spray = IconData(335, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData strawberry = IconData(336, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sweet_potato = IconData(337, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tea = IconData(338, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tetra_pak = IconData(339, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData toilet_paper = IconData(340, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tomato = IconData(341, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tooth_cleaning_kit = IconData(342, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tube = IconData(343, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData water = IconData(344, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wet = IconData(345, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wheat = IconData(346, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wine_bottle = IconData(347, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wipes = IconData(348, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
--- scripts/generate_arb.sh
+++
@@ -1,4 +0,0 @@
-# Generate arb for localization
-# Run from project folder's root
-echo Run from project root
-flutter pub run intl_translation:extract_to_arb --output-dir=lib/l10n lib/l10n/grexLocalizationDelegate.dart

d8d222ab0a736bc5133ec8766d1672c0f263ad49
Tom Bursch <tombursch@gmail.com>
2023-04-18 15:57:29 +0200

    Update API

--- lib/services/api/expense.dart
+++ lib/services/api/expense.dart
@@ -9,14 +9,20 @@ import 'package:kitchenowl/services/api/api_service.dart';
 extension ExpenseApi on ApiService {
   static const baseRoute = '/expense';
 
-  Future<List<Expense>?> getAllExpenses(
-    Household household, [
+  Future<List<Expense>?> getAllExpenses({
+    required Household household,
     ExpenselistSorting sorting = ExpenselistSorting.all,
     Expense? startAfter,
-  ]) async {
+    List<ExpenseCategory>? filter,
+  }) async {
     String url = '${householdPath(household)}$baseRoute?view=${sorting.index}';
     if (startAfter != null) {
       url += '&startAfterId=${startAfter.id}';
+    }
+    if (filter != null && filter.isNotEmpty) {
+      for (final c in filter) {
+        url += '&filter=${c.id}';
+      }
     }
 
     final res = await get(url);

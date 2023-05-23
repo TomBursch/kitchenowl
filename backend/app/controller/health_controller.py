a8939e68e2ac8466a2cde5537e515444b0fc12f8
Tom Bursch <tombursch@gmail.com>
2023-05-23 12:21:15 +0200

    feat: make health endpoint return privacy policy

--- backend/app/controller/health_controller.py
+++ backend/app/controller/health_controller.py
@@ -1,5 +1,5 @@
 from flask import jsonify, Blueprint
-from app.config import BACKEND_VERSION, MIN_FRONTEND_VERSION
+from app.config import BACKEND_VERSION, MIN_FRONTEND_VERSION, PRIVACY_POLICY_URL
 from app.models import Settings
 from app.config import SUPPORTED_LANGUAGES
 
@@ -13,6 +13,8 @@ def get_health():
         'version': BACKEND_VERSION,
         'min_frontend_version': MIN_FRONTEND_VERSION,
     }
+    if PRIVACY_POLICY_URL:
+        info['privacy_policy'] = PRIVACY_POLICY_URL
     return jsonify(info)
 
 @health.route('/supported-languages', methods=['GET'])

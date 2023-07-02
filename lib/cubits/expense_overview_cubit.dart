1c9d31ab3d934cc4f7442f6328c4fb745ff926bd
Tom Bursch <tombursch@gmail.com>
2023-07-02 14:32:16 +0200

    feat: Balance overview shows who owes whom

--- lib/cubits/expense_overview_cubit.dart
+++ lib/cubits/expense_overview_cubit.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+
+import 'package:collection/collection.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kitchenowl/enums/expenselist_sorting.dart';
 import 'package:kitchenowl/enums/timeframe.dart';
 import 'package:kitchenowl/models/expense_category.dart';
 import 'package:kitchenowl/models/household.dart';
+import 'package:kitchenowl/models/member.dart';
 import 'package:kitchenowl/services/transaction_handler.dart';
 import 'package:kitchenowl/services/transactions/expense.dart';
+import 'package:kitchenowl/services/transactions/household.dart';
 
 class ExpenseOverviewCubit extends Cubit<ExpenseOverviewState> {
   final Household household;
@@ -38,6 +43,8 @@ class ExpenseOverviewCubit extends Cubit<ExpenseOverviewState> {
 
   Future<void> _refresh() async {
     final sorting = state.sorting;
+    final fHousehold = TransactionHandler.getInstance()
+        .runTransaction(TransactionHouseholdGet(household: household));
     final categories = TransactionHandler.getInstance()
         .runTransaction(TransactionExpenseCategoriesGet(household: household));
     final overview = await TransactionHandler.getInstance()
@@ -57,9 +64,31 @@ class ExpenseOverviewCubit extends Cubit<ExpenseOverviewState> {
       sorting: sorting,
       categories: await categories,
       categoryOverviewsByCategory: overview,
+      household: (await fHousehold) ?? household,
+      owes: _calculateOwes((await fHousehold) ?? household),
     ));
 
     _refreshThread = null;
+  }
+
+  List<(Member, Member, double)> _calculateOwes(Household household) {
+    final members = household.member ?? [];
+    final memberBalances =
+        Map.fromEntries(members.map((e) => MapEntry(e, e.balance)));
+    final List<(Member, Member, double)> result = [];
+    for (final member in members) {
+      while (memberBalances[member]! < 0) {
+        final owes = members
+            .firstWhereOrNull((e) => e.balance > 0 && memberBalances[e]! > 0);
+        if (owes == null) break;
+        final amount = min(-memberBalances[member]!, memberBalances[owes]!);
+        memberBalances[member] = memberBalances[member]! + amount;
+        memberBalances[owes] = memberBalances[owes]! - amount;
+        result.add((member, owes, amount));
+      }
+    }
+
+    return result;
   }
 }
 
@@ -101,11 +130,15 @@ class ExpenseOverviewLoading extends ExpenseOverviewState {
 class ExpenseOverviewLoaded extends ExpenseOverviewState {
   final List<ExpenseCategory> categories;
   final Map<int, Map<int, double>> categoryOverviewsByCategory;
+  final Household household;
+  final List<(Member, Member, double)> owes;
 
   const ExpenseOverviewLoaded({
     required this.categories,
     required this.categoryOverviewsByCategory,
     required super.sorting,
+    required this.household,
+    required this.owes,
     super.selectedMonthIndex = 0,
   });
 
@@ -117,15 +150,25 @@ class ExpenseOverviewLoaded extends ExpenseOverviewState {
   ExpenseOverviewState copyWith({
     ExpenselistSorting? sorting,
     int? selectedMonthIndex,
+    Household? household,
+    List<(Member, Member, double)>? owes,
   }) =>
       ExpenseOverviewLoaded(
         sorting: sorting ?? this.sorting,
         categoryOverviewsByCategory: categoryOverviewsByCategory,
         categories: categories,
         selectedMonthIndex: selectedMonthIndex ?? this.selectedMonthIndex,
+        household: household ?? this.household,
+        owes: owes ?? this.owes,
       );
 
   @override
-  List<Object?> get props =>
-      [sorting, selectedMonthIndex, categories, categoryOverviewsByCategory];
+  List<Object?> get props => [
+        sorting,
+        selectedMonthIndex,
+        categories,
+        categoryOverviewsByCategory,
+        household,
+        owes,
+      ];
 }

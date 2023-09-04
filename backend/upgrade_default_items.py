fac955a3cf57d24ed1d020552b4213a6ec587534
Tom Bursch <tombursch@gmail.com>
2023-09-04 15:20:22 +0200

    fix: print household upgrade progress

--- backend/upgrade_default_items.py
+++ backend/upgrade_default_items.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from app import app
 from app.errors import NotFoundRequest
 from app.models import Household
@@ -6,7 +7,7 @@ from app.service.import_language import importLanguage
 
 if __name__ == "__main__":
     with app.app_context():
-        for household in Household.query.filter(Household.language != None).all():
+        for household in tqdm(Household.query.filter(Household.language != None).all(), desc="Upgrading households..."):
             try:
                 importLanguage(household.id, household.language, bulkSave=True)
             except NotFoundRequest:

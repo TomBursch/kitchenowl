d14692bfe3a97e41b3d35d0a11d660214cdad0ca
Tom Bursch <tombursch@gmail.com>
2023-10-03 18:14:34 +0200

    Allow skip of default item upgrade

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -p $STORAGE_PATH/upload
 flask db upgrade
-python upgrade_default_items.py
+if [ "${SKIP_UPGRADE_DEFAULT_ITEMS,,}" != "true" ]; then
+    python upgrade_default_items.py
+fi
 uwsgi "$@"

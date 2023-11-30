8533de626138ba948d6df53bfe2cc5fa4582d2f5
Tom Bursch <tombursch@gmail.com>
2023-11-30 12:37:31 +0100

    fix: Upgrade default items

--- backend/upgrade_default_items.py
+++ backend/upgrade_default_items.py
@@ -1,12 +1,14 @@
 from tqdm import tqdm
+from app import app
 from app.errors import NotFoundRequest
 from app.models import Household
 from app.service.import_language import importLanguage
 
 
 if __name__ == "__main__":
-    for household in tqdm(Household.query.filter(Household.language != None).all(), desc="Upgrading households..."):
-        try:
-            importLanguage(household.id, household.language, bulkSave=True)
-        except NotFoundRequest:
-            pass
+    with app.app_context():
+        for household in tqdm(Household.query.filter(Household.language != None).all(), desc="Upgrading households"):
+            try:
+                importLanguage(household.id, household.language, bulkSave=True)
+            except NotFoundRequest:
+                pass

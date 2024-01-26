3cfceef2f1c74cba9a342bdc9f9afaf47882efc7
Tom Bursch <tombursch@gmail.com>
2024-01-26 17:26:09 +0100

    lang(backend): Add Bengali

--- backend/app/config.py
+++ backend/app/config.py
@@ -75,6 +75,7 @@ APPLE_CLIENT_SECRET = os.getenv("APPLE_CLIENT_SECRET")
 SUPPORTED_LANGUAGES = {
     "en": "English",
     "en_AU": "Australian English",
+    "bn": "বাংলা",
     "cs": "čeština",
     "da": "Dansk",
     "de": "Deutsch",

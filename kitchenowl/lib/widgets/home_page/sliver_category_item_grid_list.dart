f7e0e658399d418c961449a8e9c93e278b920e33
rmax1024 <7482819+rmax1024@users.noreply.github.com>
2024-03-01 04:48:38 -0500

    feat: Categorize recent items #149 (#404)

--- kitchenowl/lib/widgets/home_page/sliver_category_item_grid_list.dart
+++ kitchenowl/lib/widgets/home_page/sliver_category_item_grid_list.dart
@@ -20,6 +20,8 @@ class SliverCategoryItemGridList<T extends Item> extends StatefulWidget {
   final bool isLoading;
   final bool? allRaised;
   final Widget Function(T)? extraOption;
+  final bool isSubTitle;
+  final bool splitByCategories;
 
   const SliverCategoryItemGridList({
     super.key,
@@ -35,6 +37,8 @@ class SliverCategoryItemGridList<T extends Item> extends StatefulWidget {
     this.isLoading = false,
     this.allRaised,
     this.extraOption,
+    this.isSubTitle = false,
+    this.splitByCategories = false,
   });
 
   @override
@@ -48,6 +52,53 @@ class _SliverCategoryItemGridListState<T extends Item>
 
   @override
   Widget build(BuildContext context) {
+    TextStyle? titleTextStyle = Theme.of(context).textTheme.titleLarge;
+    if (widget.isSubTitle)
+      titleTextStyle = titleTextStyle?.apply(fontStyle: FontStyle.italic, fontWeightDelta: -1);
+
+    List<Widget> list = [];
+    final categoryLength = widget.categories?.length ?? 0;
+
+    if (widget.splitByCategories) {
+      for (int i = 0; i < categoryLength + 1; i++) {
+        Category? category = i < categoryLength
+            ? widget.categories![i]
+            : null;
+        final List<T> items = widget.items
+            .where((e) => e.category == category)
+            .toList();
+        if (items.isEmpty) continue;
+
+        list.add(SliverCategoryItemGridList(
+          name: category?.name ??
+              AppLocalizations.of(context)!.uncategorized,
+          items: items,
+          categories: widget.categories,
+          shoppingList: widget.shoppingList,
+          selected: widget.selected,
+          isLoading: widget.isLoading,
+          onRefresh: widget.onRefresh,
+          onPressed: widget.onPressed,
+          isSubTitle: true,
+          allRaised: widget.allRaised,
+          extraOption: widget.extraOption,
+        ));
+      }
+    }
+    else
+      list.add(SliverItemGridList<T>(
+        onRefresh: widget.onRefresh,
+        onPressed: widget.onPressed,
+        onLongPressed: widget.onLongPressed,
+        items: widget.items,
+        categories: widget.categories,
+        shoppingList: widget.shoppingList,
+        selected: widget.selected,
+        isLoading: widget.isLoading,
+        allRaised: widget.allRaised,
+        extraOption: widget.extraOption,
+      ));
+
     return SliverMainAxisGroup(
       slivers: [
         SliverToBoxAdapter(
@@ -64,7 +115,7 @@ class _SliverCategoryItemGridListState<T extends Item>
                   Expanded(
                     child: Text(
                       widget.name,
-                      style: Theme.of(context).textTheme.titleLarge,
+                      style: titleTextStyle,
                     ),
                   ),
                   IconButton(
@@ -86,18 +137,7 @@ class _SliverCategoryItemGridListState<T extends Item>
           duration: widget.animationDuration,
           child: !isExpanded
               ? const SliverToBoxAdapter(child: SizedBox())
-              : SliverItemGridList<T>(
-                  onRefresh: widget.onRefresh,
-                  onPressed: widget.onPressed,
-                  onLongPressed: widget.onLongPressed,
-                  items: widget.items,
-                  categories: widget.categories,
-                  shoppingList: widget.shoppingList,
-                  selected: widget.selected,
-                  isLoading: widget.isLoading,
-                  allRaised: widget.allRaised,
-                  extraOption: widget.extraOption,
-                ),
+              : MultiSliver(children: list),
         ),
       ],
     );

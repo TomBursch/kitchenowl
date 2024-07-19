ba76af1ed556983a13de43b933e38b9225463626
Tom Bursch <tombursch@gmail.com>
2024-07-19 15:57:21 +0200

    Add bulgarian (#473)

--- backend/app/config.py
+++ backend/app/config.py
@@ -76,6 +76,7 @@ APPLE_CLIENT_SECRET = os.getenv("APPLE_CLIENT_SECRET")
 SUPPORTED_LANGUAGES = {
     "en": "English",
     "en_AU": "Australian English",
+    "bg": "български език",
     "bn": "বাংলা",
     "ca": "Catalan",
     "cs": "čeština",

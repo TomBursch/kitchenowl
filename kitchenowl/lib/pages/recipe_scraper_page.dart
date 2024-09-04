b8fe3add30d2370e0d3514aae2a976dc4fc6773d
w0s1nsk1 <tomek12wosin@gmail.com>
2024-09-04 17:56:50 +0200

    feat: Update scrape failure screens

--- kitchenowl/lib/pages/recipe_scraper_page.dart
+++ kitchenowl/lib/pages/recipe_scraper_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kitchenowl/app.dart';
 import 'package:kitchenowl/cubits/household_cubit.dart';
 import 'package:kitchenowl/cubits/recipe_scraper_cubit.dart';
 import 'package:kitchenowl/enums/update_enum.dart';
+import 'package:kitchenowl/helpers/url_launcher.dart';
 import 'package:kitchenowl/kitchenowl.dart';
 import 'package:kitchenowl/models/household.dart';
 import 'package:kitchenowl/pages/recipe_add_update_page.dart';
+import 'package:kitchenowl/styles/color_mapper.dart';
 import 'package:kitchenowl/widgets/string_item_match.dart';
 
 class RecipeScraperPage extends StatefulWidget {
@@ -47,21 +51,101 @@ class _RecipeScraperPageState extends State<RecipeScraperPage> {
           bloc: cubit,
           builder: (context, state) {
             if (state is! RecipeScraperLoadedState) {
+              late final Widget body;
+              if (state is RecipeScraperErrorState) {
+                body = Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 60,
+                          vertical: 25,
+                        ),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: SvgPicture(
+                              SvgAssetLoader(
+                                "assets/illustrations/right_direction.svg",
+                                colorMapper: KitchenOwlColorMapper(
+                                  accentColor:
+                                      Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                              fit: BoxFit.scaleDown,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        AppLocalizations.of(context)!.error,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      const SizedBox(height: 8),
+                      if (!App.isDefaultServer)
+                        ElevatedButton(
+                          onPressed: () => openUrl(
+                            context,
+                            "https://github.com/TomBursch/kitchenowl/issues/new/choose",
+                          ),
+                          child:
+                              Text(AppLocalizations.of(context)!.reportIssue),
+                        )
+                    ],
+                  ),
+                );
+              } else if (state is RecipeScraperUnsupportedState) {
+                body = Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 60,
+                          vertical: 25,
+                        ),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: SvgPicture(
+                              SvgAssetLoader(
+                                "assets/illustrations/under_construction.svg",
+                                colorMapper: KitchenOwlColorMapper(
+                                  accentColor:
+                                      Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                              fit: BoxFit.scaleDown,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        AppLocalizations.of(context)!.unsupportedScrapeMessage,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                body = const Center(child: CircularProgressIndicator());
+              }
+
               return Scaffold(
                 appBar: AppBar(
                   title: Text(
-                    Uri.tryParse(widget.url)?.host ?? widget.url,
+                    Uri.tryParse(widget.url)?.host.isNotEmpty ?? false
+                        ? Uri.tryParse(widget.url)!.host
+                        : widget.url,
                     overflow: TextOverflow.fade,
                   ),
                 ),
-                body: Center(
-                  child: state is RecipeScraperErrorState
-                      ? Text(AppLocalizations.of(context)!.error)
-                      : state is RecipeScraperUnsupportedState
-                          ? Text(AppLocalizations.of(context)!
-                              .unsupportedScrapeMessage)
-                          : const CircularProgressIndicator(),
-                ),
+                body: body,
               );
             }
 

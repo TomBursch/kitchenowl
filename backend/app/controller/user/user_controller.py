abf2c56e02745318f0589131f2a2c3ede16ba457
Tom Bursch <tombursch@gmail.com>
2024-09-13 15:05:07 +0200

    feat: Set profile picture

--- backend/app/controller/user/user_controller.py
+++ backend/app/controller/user/user_controller.py
@@ -93,6 +93,7 @@ def updateUser(args):
                 ChallengeMailVerify.create_challenge(user),
             )
     if "photo" in args and user.photo != args["photo"]:
+        print(args["photo"])
         user.photo = file_has_access_or_download(args["photo"], user.photo)
     user.save()
     return jsonify({"msg": "DONE"})

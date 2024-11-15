62cff91f372e229bfe767f86c9e8a20261e30db1
w0s1nsk1 <tomek12wosin@gmail.com>
2024-11-15 18:06:55 +0100

    feat: Use cupertino client on iOS & Mac

--- kitchenowl/lib/services/api/api_service.dart
+++ kitchenowl/lib/services/api/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:cronet_http/cronet_http.dart';
+import 'package:cupertino_http/cupertino_http.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
@@ -79,6 +80,14 @@ class ApiService {
             "KitchenOwl-${Platform.operatingSystem}/${Config.packageInfoSync?.version}",
       );
       _client = CronetClient.fromCronetEngine(engine, closeEngine: true);
+    } else if (!kIsWeb && (Platform.isIOS || Platform.isMacOS)) {
+      final config = URLSessionConfiguration.ephemeralSessionConfiguration()
+        ..cache = URLCache.withCapacity(memoryCapacity: 2 * 1024 * 1024)
+        ..httpAdditionalHeaders = {
+          "User-Agent":
+              "KitchenOwl-${Platform.operatingSystem}/${Config.packageInfoSync?.version}"
+        };
+      _client = CupertinoClient.fromSessionConfiguration(config);
     } else if (!kIsWeb) {
       _client = IOClient(HttpClient()
         ..userAgent =

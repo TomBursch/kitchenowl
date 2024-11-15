a665c914c7a4dcad44cf78bae0bd1a2a6578c301
Tom Bursch <tombursch@gmail.com>
2024-11-15 13:56:18 +0100

    feat: Use cronet client on android

--- kitchenowl/lib/services/api/api_service.dart
+++ kitchenowl/lib/services/api/api_service.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:cronet_http/cronet_http.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 import 'package:kitchenowl/config.dart';
 import 'package:kitchenowl/helpers/named_bytearray.dart';
 import 'package:kitchenowl/models/household.dart';
@@ -49,7 +52,7 @@ class ApiService {
   String householdPath(Household household) => "/household/${household.id}";
 
   static ApiService? _instance;
-  final _client = http.Client();
+  late final http.Client _client;
   late final Socket socket;
   final String baseUrl;
   String? _refreshToken;
@@ -68,6 +71,18 @@ class ApiService {
   ApiService._internal(String baseUrl)
       : baseUrl = baseUrl.isNotEmpty ? baseUrl + _API_PATH : "" {
     _connectionNotifier.value = Connection.undefined;
+    if (!kIsWeb && Platform.isAndroid) {
+      final engine = CronetEngine.build(
+        cacheMode: CacheMode.memory,
+        cacheMaxSize: 2 * 1024 * 1024,
+        userAgent: "KitchenOwl Agent",
+      );
+      _client = CronetClient.fromCronetEngine(engine, closeEngine: true);
+    } else if (!kIsWeb) {
+      _client = IOClient(HttpClient()..userAgent = "KitchenOwl Agent");
+    } else {
+      _client = http.Client();
+    }
     socket = io(
       baseUrl,
       OptionBuilder()

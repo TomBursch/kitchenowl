4bdda5a815c695b4a93445a370b232b720e787c4
Tom Bursch <tombursch@gmail.com>
2024-11-23 18:57:31 +0100

    Prepare release v0.6.2

--- backend/app/config.py
+++ backend/app/config.py
@@ -33,7 +33,7 @@ import os
 
 
 MIN_FRONTEND_VERSION = 71
-BACKEND_VERSION = 99
+BACKEND_VERSION = 100
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(APP_DIR)

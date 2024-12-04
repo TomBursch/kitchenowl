65e73eace24b3607c2a374fc764b5c0899f31df6
Julian Merkle <juli.merkle@gmail.com>
2024-12-04 14:02:30 +0100

    feat: patch env BASE_HREF into index.html
    This updates the default base href="/"> node with the BASE_HREF environment variable.
    
    closes #130

--- kitchenowl/docker-entrypoint-custom.sh
+++ kitchenowl/docker-entrypoint-custom.sh
@@ -14,3 +14,18 @@ if [ ! -f "/proc/net/if_inet6" ]; then
     entrypoint_log "$ME: info: ipv6 not available"
     sed -i '/::/d' /etc/nginx/templates/default.conf.template
 fi
+
+# patch the web base href if requested
+if [ ! -z $BASE_HREF ]; then
+    if [[ $BASE_HREF == *"#"* ]]; then
+        echo "BASE_HREF must not contain character '#'" >&2
+        exit 1
+    elif [[ $BASE_HREF != "/"* ]]; then
+        echo "BASE_HREF must begin with a forward slash: /example/" >&2
+        exit 1
+    elif [[ $BASE_HREF != *"/" ]]; then
+        echo "BASE_HREF must end with a forward slash: /example/" >&2
+        exit 1
+    fi
+    sed -i "s#<base href=\"/\">#<base href=\"${BASE_HREF}\">#g" /var/www/web/kitchenowl/index.html
+fi

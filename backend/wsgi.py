3b46c79d84241961cff95221ccb17d5718bf2363
w0s1nsk1 <tomek12wosin@gmail.com>
2024-12-13 11:32:00 +0100

    forward args to flask from wsgi (#573)
    Co-authored-by: Guilherme Bufolo <guibufolo+hobbyhimmel@gmail.com>

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,12 +1,21 @@
 import gevent.monkey
 gevent.monkey.patch_all()
-
-from app import app, socketio, celery_app
+import argparse
 import os
 
+from app import app, socketio
 from app.config import UPLOAD_FOLDER
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", required=False, default=None, type=str, nargs="?", help="Set the host to use. Forwarded to flask.")
+    parser.add_argument("--debug", required=False, default=True, type=bool, help="Set the debug flag. Forwarded to flask.")
+
+    return parser.parse_args()
+    
+
 if __name__ == "__main__":
+    arguments = parse_arguments()
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=arguments.debug, host=arguments.host)

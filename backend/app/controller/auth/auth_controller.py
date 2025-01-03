3c1f9820ce232cee3691d394a567e40123302346
w0s1nsk1 <tomek12wosin@gmail.com>
2025-01-03 11:45:46 +0100

    fix: Minor

--- backend/app/controller/auth/auth_controller.py
+++ backend/app/controller/auth/auth_controller.py
@@ -36,6 +36,7 @@ def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
         token.save()
     return token is None
 
+
 # Register a callback function that takes whatever object is passed in as the
 # identity when creating JWTs and converts it to a JSON serializable format.
 @jwt.user_identity_loader

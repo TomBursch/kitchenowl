3ab5d8e3f1e94609a26ab2eba1f226a5d5f16388
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-02-21 19:39:31 +0100

    return days for backward compatibility

--- backend/app/controller/planner/planner_controller.py
+++ backend/app/controller/planner/planner_controller.py
@@ -11,9 +11,21 @@ import warnings
 
 plannerHousehold = Blueprint("planner", __name__)
 
+def is_within_next_7_days(target_date: datetime) -> bool:
+    # Get the current date and time
+    now = datetime.now()
+    
+    # Calculate the date 7 days from now
+    seven_days_later = now + timedelta(days=7)
+    
+    # Check if the target date is within the next 7 days
+    return now <= target_date <= seven_days_later
+
+def transform_when_to_day(when: datetime) -> int:
+    if is_within_next_7_days(when):
+        return when.weekday()
 
 def next_weekday(weekday_number: int) -> datetime:
-    warnings.warn("deprecated", DeprecationWarning)
     # Get today's date
     today = datetime.now()
     
@@ -24,6 +36,7 @@ def next_weekday(weekday_number: int) -> datetime:
     next_date = today + timedelta(days=days_ahead)
     
     return next_date
+
 
 
 @plannerHousehold.route("/recipes", methods=["GET"])
@@ -47,7 +60,11 @@ def getAllPlannedRecipes(household_id):
 @authorize_household()
 def getPlanner(household_id):
     plans = Planner.all_from_household(household_id)
-    return jsonify([e.obj_to_full_dict() for e in plans])
+    k = [e.obj_to_full_dict() for e in plans]
+    # add day for backwards compatibility
+    for d in k:
+        d["day"] = transform_when_to_day(d["when"])
+    return jsonify(k)
 
 
 @plannerHousehold.route("/recipe", methods=["POST"])
@@ -61,8 +78,7 @@ def addPlannedRecipe(args, household_id):
     when = args["when"] if "when" in args else datetime.min
     if "day" in args:
         # if outdated "day" was used, transform it into next date with that weekday
-        when = next_weekday(args["day"]).replace(hour=23,minute=59, second=59)
-        print(f"WHEN: {when}")
+        when = next_weekday(args["day"]).replace(hour=23,minute=59, second=59, microsecond=0)
     planner = Planner.find_by_datetime(household_id=household_id, recipe_id=recipe.id, when=when)
     if not planner:
         if when > datetime.min:
@@ -96,7 +112,7 @@ def removePlannedRecipeById(args, household_id, id):
     when = args["when"] if "when" in args else datetime.min
     if "day" in args:
         # if outdated "day" was used, transform it into next date with that weekday
-        when = next_weekday(args["day"])
+        when = next_weekday(args["day"]).replace(hour=23,minute=59, second=59, microsecond=0)
     planner = Planner.find_by_datetime(household_id, recipe_id=recipe.id, when=when)
     if planner:
         planner.delete()

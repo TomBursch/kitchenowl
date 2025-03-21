1dc575c85cef0e70207bfbb578b986f4d81b943c
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-03-21 10:27:25 +0100

    remove backward compatibility and raise MIN_BACKEND_VERSION

--- kitchenowl/lib/models/planner.dart
+++ kitchenowl/lib/models/planner.dart
@@ -3,20 +3,6 @@ import 'package:kitchenowl/models/recipe.dart';
 
 final datetime_min = DateTime(1, 1, 1);
 
-DateTime nextWeekday(int targetWeekday) {
-  DateTime now = DateTime.now();
-  
-  int currentWeekday = now.weekday - 1;
-
-  // Calculate the difference
-  int daysToAdd = (targetWeekday - currentWeekday) % 6;
-  if (daysToAdd < 0) {
-    daysToAdd += 7; // Move to the next week if the target day is today or in the past
-  }
-
-  // Return the next possible DateTime for the target weekday
-  return now.add(Duration(days: daysToAdd));
-}
 DateTime toEndOfDay(DateTime dt) {
   return DateTime(dt.year, dt.month, dt.day, 23, 59, 59);
 }
@@ -25,32 +11,18 @@ DateTime toEndOfDay(DateTime dt) {
 class RecipePlan extends Model {
   final Recipe recipe;
   final DateTime? cooking_date; // privat gemacht
-  final int? _day;
   final int? yields;
 
   RecipePlan({
     required this.recipe,
     this.cooking_date, // Parameter umbenennen
     this.yields,
-  }) : _day = cooking_date != null ? cooking_date.weekday : null; 
-
-  int? get day => _day;
+  }); 
 
   factory RecipePlan.fromJson(Map<String, dynamic> map) {
-    DateTime? _cooking_date;
-    if (map.containsKey("day") && map['day'] != null) {
-      if (map['day'] == -1) {
-        _cooking_date = datetime_min;
-      } else {
-        _cooking_date = toEndOfDay(nextWeekday(map['day'])); 
-      }
-    }
-    if (map.containsKey("cooking_date")) {
-      _cooking_date = DateTime.fromMillisecondsSinceEpoch(map["cooking_date"], isUtc: true); 
-    }
     return RecipePlan(
       recipe: Recipe.fromJson(map['recipe']),
-      cooking_date: _cooking_date,
+      cooking_date: DateTime.fromMillisecondsSinceEpoch(map["cooking_date"], isUtc: true),
       yields: map['yields'],
     );
   }

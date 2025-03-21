129a24802066568ae46e94610d7d2862bddd06ab
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-03-21 09:41:47 +0100

    raise backend-version

--- backend/app/config.py
+++ backend/app/config.py
@@ -33,7 +33,7 @@ import os
 
 
 MIN_FRONTEND_VERSION = 71
-BACKEND_VERSION = 108
+BACKEND_VERSION = 109
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(APP_DIR)

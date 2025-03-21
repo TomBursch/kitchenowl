4bcbdce4d487e2a1ebade71e2b9e2871c65ca6a8
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-03-21 18:47:11 +0100

    remove unused import

--- kitchenowl/lib/pages/recipe_page.dart
+++ kitchenowl/lib/pages/recipe_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 import 'package:kitchenowl/app.dart';
 import 'package:kitchenowl/cubits/recipe_cubit.dart';
 import 'package:kitchenowl/enums/update_enum.dart';

0630ca5e6d2560a6c5c6dcd3011351183d3051e8
w0s1nsk1 <tomek12wosin@gmail.com>
2025-04-22 20:53:34 +0200

    Go to members on avatar list tap

--- kitchenowl/lib/widgets/household_image.dart
+++ kitchenowl/lib/widgets/household_image.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:kitchenowl/models/household.dart';
+import 'package:kitchenowl/pages/household_member_page.dart';
 import 'package:kitchenowl/widgets/avatar_list.dart';
 import 'package:kitchenowl/widgets/image_provider.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class HouseholdImage extends StatelessWidget {
   final Household household;
+  final bool enableMembersTap;
 
-  const HouseholdImage({super.key, required this.household});
+  const HouseholdImage({
+    super.key,
+    required this.household,
+    this.enableMembersTap = true,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +43,15 @@ class HouseholdImage extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: AvatarList(
                   users: household.member!,
+                  onTap: enableMembersTap
+                      ? () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (ctx) => HouseholdMemberPage(
+                                household: household,
+                              ),
+                            ),
+                          )
+                      : null,
                 ),
               ),
           ],

9b5d2fbc56ff2152d52a1c9da8a601eac98c9f5d
w0s1nsk1 <tomek12wosin@gmail.com>
2025-05-12 21:34:51 +0200

    Prepare release v0.6.15

--- backend/app/config.py
+++ backend/app/config.py
@@ -33,7 +33,7 @@ import os
 
 
 MIN_FRONTEND_VERSION = 71
-BACKEND_VERSION = 112
+BACKEND_VERSION = 113
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(APP_DIR)

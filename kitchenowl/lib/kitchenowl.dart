7579232788c695c5475146fce290ce79c9df43fd
w0s1nsk1 <tomek12wosin@gmail.com>
2025-05-22 21:20:22 +0200

    chore: Upgrade dependencies

--- kitchenowl/lib/kitchenowl.dart
+++ kitchenowl/lib/kitchenowl.dart
@@ -1,4 +1,4 @@
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export 'package:kitchenowl/gen/l10n/app_localizations.dart';
 export 'widgets/_export.dart';
 export 'models/nullable.dart';
 export 'styles/dynamic.dart';

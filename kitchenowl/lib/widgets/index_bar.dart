886d13833e38c2ec9799813999507ad86a2333ab
Tom Bursch <tombursch@gmail.com>
2025-06-21 17:52:57 +0200

    Use light vibration for recipe index bar

--- kitchenowl/lib/widgets/index_bar.dart
+++ kitchenowl/lib/widgets/index_bar.dart
@@ -476,7 +476,7 @@ class _BaseIndexBarState extends State<BaseIndexBar> {
     if (widget.hapticFeedback &&
         (action == IndexBarDragDetails.actionDown ||
             action == IndexBarDragDetails.actionUpdate)) {
-      HapticFeedback.vibrate();
+      HapticFeedback.lightImpact();
     }
     widget.indexBarDragNotifier?.dragDetails.value = IndexBarDragDetails(
       action: action,

b58a75339a57cadf94bf6b2e1afd95d7cf3d07c0
CH3T1NG4L1F3 <justin_rieck@t-online.de>
2026-01-01 15:01:13 +0100

    added new enum ListStyle and new styleClass for shoppinglistview

--- kitchenowl/lib/styles/dynamic.dart
+++ kitchenowl/lib/styles/dynamic.dart
@@ -4,7 +4,26 @@ enum GridSize {
   large,
 }
 
+enum ListStyle {
+  minimalist,
+  cards
+}
+
 class DynamicStyling {
   static int itemCrossAxisCount(double availableSpace, GridSize? sizing) =>
       (availableSpace ~/ 115).clamp(1, 9) - ((sizing?.index ?? 1) - 1);
+}
+
+class ShoppingListStyle {
+  final bool advancedItemView;
+  final bool isList;
+  final GridSize gridSize;
+  final ListStyle listStyle;
+
+  const ShoppingListStyle({
+    this.advancedItemView = false,
+    this.isList = false,
+    this.gridSize = GridSize.normal,
+    this.listStyle = ListStyle.minimalist,
+  });
 }

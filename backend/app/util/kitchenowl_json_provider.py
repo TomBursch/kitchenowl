50ae98aec1ad7046cd7b95637c50e00704f9c636
w0s1nsk1 <tomek12wosin@gmail.com>
2026-01-11 00:09:28 +0100

    fix json provider

--- backend/app/util/kitchenowl_json_provider.py
+++ backend/app/util/kitchenowl_json_provider.py
@@ -1,11 +1,11 @@
 import enum
 from flask.json.provider import DefaultJSONProvider
-from datetime import date, timezone
+from datetime import date, datetime, timezone
 
 
 class KitchenOwlJSONProvider(DefaultJSONProvider):
     def default(self, o):  # type: ignore[assignment]
-        if isinstance(o, date):
+        if isinstance(o, (datetime, date)):
             return int(round(o.replace(tzinfo=timezone.utc).timestamp() * 1000))
         if isinstance(o, enum.Enum):
             return int(o.value)

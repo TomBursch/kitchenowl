7735c6b6e2fe2f94908d507c9076f6c2b21173d7
Tom Bursch <tombursch@gmail.com>
2026-04-03 01:25:14 +0200

    Add Estonian

--- backend/app/config.py
+++ backend/app/config.py
@@ -109,6 +109,7 @@ SUPPORTED_LANGUAGES = {
     "de_CH": "Deutsch (Schweiz)",
     "el": "Ελληνικά",
     "es": "Español",
+    "et": "eesti keel",
     "fa": "فارسی",
     "fi": "Suomi",
     "fr": "Français",
